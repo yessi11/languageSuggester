41de80d377f090f7c6edb2366c39884b46476783
yesenia reyes <yeseniareyes5@gmail.com>
2022-01-14 18:14:41 -0600

    function to toggle between text

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 //submit form for id name
-$("form#insurance").submit(function(event) {
+  $("form#name0form").submit(function(event) {
   const name = $("input#fullname").val();
     
-  $("#name1").text(name);
-  $("#username").show();
+  $("#username").text(name);
+  $("#name1").show();
   
   event.preventDefault();
-});
+  });
   
 //once name input will move on to questions
   $(".clickable").click(function() {
     $(".showAsk").toggle();
+    $(".hideAsk").toggle();
   });
+
+//results for questionaire 
+  $  
 
 
 });
